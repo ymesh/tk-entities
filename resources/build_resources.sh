6b7af3488db3f1bee9110b535fc47d2cea3f3c22
Yuri Meshalkin <ymesh@me.com>
2020-01-22 19:07:41 +0200

    edit app location for building resources

--- resources/build_resources.sh
+++ resources/build_resources.sh
@@ -11,7 +11,7 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 # The path to output all built .py files to: 
-UI_PYTHON_PATH=../python/app/ui
+UI_PYTHON_PATH=../python/tk_entities/ui
 
 
 # Helper functions to build UI files
